9481617743c387c62438aaa2a480ecbefd80adc5
chris <caconstan@yahoo.com>
2020-05-21 11:07:05 -0400

    commit Thurs morning

---
+++ MovieRegistrationLab21/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MovieRegistrationLab21.Models;
+
+namespace MovieRegistrationLab21.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult RegisterMovie()
+        {
+            return View();
+        }
+
+        public IActionResult EditMovie(MovieModel mm)
+        {
+           return View("RegisterMovie", mm);
+        }
+
+        public IActionResult RegisterMovieSummary(MovieModel mm)
+        {
+            if (ModelState.IsValid )
+            {
+                return View(mm);
+            } else
+            {
+                return View("RegisterMovie", mm);
+            }
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MovieRegistrationLab21/Models/MovieModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace MovieRegistrationLab21.Models
+{
+    public class MovieModel
+    {
+        [Required(ErrorMessage = "Please enter a unique identifier for this movie")]
+        public string ID { get; set; }
+        [Required]
+        [StringLength(10, MinimumLength = 3, ErrorMessage = "Title must be between 3 and 10 characters")]
+        public string Title { get; set; }
+        [Required] 
+        public string Genre { get; set; }
+
+        [Range(1900, 2020, ErrorMessage = "Year must be between 1900 and 2020")]
+        public int Year { get; set; }
+        [Required] 
+        public string Actors { get; set; }
+        [Required] 
+        public string Directors { get; set; }
+    }
+}
